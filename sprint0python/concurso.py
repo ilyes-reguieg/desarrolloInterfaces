2adb1d020fd6b1770978da08d220901879e5559e
ilyes-reguieg <ireguieg22@fpcoruna.afundacion.org>
2022-09-27 09:00:02 +0200

    Completada tarea 1 de  sprint0python

---
+++ sprint0python/concurso.py
@@ -0,0 +1,5 @@
+answer = input('Cual es la capital de Cameroon? \n a): Madrid \n b): Mingasa \n c): Douala \n ')
+if(answer == 'c'):
+    print('Respuesta correcta')
+else:
+    print('Respuesta incorrecta')

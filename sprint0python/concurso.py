1629819813b536c5bdbbe9d76ecb545229782477
ilyes-reguieg <ireguieg22@fpcoruna.afundacion.org>
2022-09-27 09:18:40 +0200

    Completada tarea 2 de  sprint0python

--- sprint0python/concurso.py
+++ sprint0python/concurso.py
@@ -1,5 +1,29 @@
-answer = input('Cual es la capital de Cameroon? \n a): Madrid \n b): Mingasa \n c): Douala \n ')
-if(answer == 'c'):
+pregunta1 = input('Cual es la capital de Cameroon? \n a): Madrid \n b): Mingasa \n c): Douala \n ')
+notaFinal = 0
+
+if(pregunta1 == 'c'):
     print('Respuesta correcta')
+    notaFinal = notaFinal + 10
 else:
     print('Respuesta incorrecta')
+    notaFinal = notaFinal - 5
+
+    
+pregunta2 = input('Cual de las siguientes tecnologias no se considera como lenguaje de programacion? \n a): Html \n b): Java \n c): Python \n')
+if(pregunta2 == 'a'):
+    print('Respuesta correcta')
+    notaFinal = notaFinal + 10
+else:
+    print('Respuesta incorrecta')
+    notaFinal = notaFinal - 5
+    
+pregunta3 = input('Cual es la nacionalidad del futbolista Granit Xhaka?  \n a): Suiza \n b): Inglatrra \n c): Japon \n')
+if(pregunta3 == 'a'):
+    print('Respuesta correcta')
+    notaFinal = notaFinal + 10
+else:
+    print('Respuesta incorrecta')
+    notaFinal = notaFinal - 5
+     
+print('tu nota final es: ' + str(notaFinal))
+     

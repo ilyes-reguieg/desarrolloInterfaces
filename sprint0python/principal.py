a238dbb1b1c06b19ce95c8d7ab7fa7471c19eee5
ilyes-reguieg <ireguieg22@fpcoruna.afundacion.org>
2022-09-28 08:52:43 +0200

    Completada tarea 5 de  sprint0python

--- sprint0python/principal.py
+++ sprint0python/principal.py
@@ -1,4 +1,8 @@
 from math import *
 from fibonacci2 import *
+from fibonacci import *
 
-funcion_fibonacci2()
+answer = input('que opcion quieres eligir tecla a si quieres ejecutar con fibo1  tecla b si quieres con fibo 2 ')
+
+if(answer == 'b'):funcion_fibonacci2()
+if(answer == 'a'):funcion_fibonacci()

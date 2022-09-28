4c29772f85697bfffdf86b1ced746d13d17ce0e4
ilyes-reguieg <ireguieg22@fpcoruna.afundacion.org>
2022-09-28 08:56:12 +0200

    Completada tarea 6 de  sprint0python

--- sprint0python/principal.py
+++ sprint0python/principal.py
@@ -1,8 +1,16 @@
+import time
+
 from math import *
 from fibonacci2 import *
 from fibonacci import *
 
 answer = input('que opcion quieres eligir tecla a si quieres ejecutar con fibo1  tecla b si quieres con fibo 2 ')
 
-if(answer == 'b'):funcion_fibonacci2()
+if(answer == 'b'):
+    start_time = time.time()
+    funcion_fibonacci2()
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
+    
 if(answer == 'a'):funcion_fibonacci()
